2c76164ac39d2e1f1b31cc57046da56e2b85af14
huydd29081999 <do.duc.huy@sun-asterisk.com>
2021-05-07 15:26:22 +0700

    Task35958

---
+++ app/src/main/java/com/sun/dummyshop/base/BaseFragment.kt
@@ -0,0 +1,50 @@
+package com.sun.dummyshop.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import com.sun.dummyshop.utils.showToast
+
+abstract class BaseFragment<V : ViewDataBinding> : Fragment() {
+
+    @get: LayoutRes
+    protected abstract val layoutResource: Int
+    protected var binding: V? = null
+    abstract val viewModel: BaseViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = DataBindingUtil
+        .inflate<V>(inflater, layoutResource, container, false)
+        .apply { binding = this }
+        .root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.error.observe(viewLifecycleOwner, Observer {
+            view.context.showToast(it)
+        })
+        setupViews()
+        setupData()
+        setupActions()
+    }
+
+    protected abstract fun setupViews()
+
+    protected abstract fun setupData()
+
+    protected abstract fun setupActions()
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
