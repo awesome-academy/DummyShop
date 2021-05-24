a70a26a0204d9c884cfee14cf888e7fd7cd3f4e6
huydd29081999 <do.duc.huy@sun-asterisk.com>
2021-05-24 10:06:14 +0700

    Task35949

--- app/src/main/java/com/sun/dummyshop/ui/main/MainActivity.kt
+++ app/src/main/java/com/sun/dummyshop/ui/main/MainActivity.kt
@@ -1,23 +1,34 @@
 package com.sun.dummyshop.ui.main
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.sun.dummyshop.R
+import com.sun.dummyshop.utils.NetworkUtil
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    private var isConnection = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.DummyShopTheme)
+        isConnection = NetworkUtil.isConnection(this)
+        if (!isConnection) {
+            showDialogCheckInternet().show()
+            return
+        }
         setContentView(R.layout.activity_main)
 
         val navigationHostFragment =
@@ -43,6 +54,20 @@ class MainActivity : AppCompatActivity() {
             imm.hideSoftInputFromWindow(it.windowToken, 0)
         }
         return super.dispatchTouchEvent(ev)
+    }
+
+    private fun showDialogCheckInternet(): AlertDialog {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.title_internet))
+            .setMessage(getString(R.string.text_message_internet))
+            .setPositiveButton(getString(R.string.text_ok)) { _, _ ->
+                startActivity(Intent(Settings.ACTION_WIFI_SETTINGS))
+                finish()
+            }
+            .setNegativeButton(getString(R.string.text_cancel)) { _, _ ->
+                finish()
+            }
+        return builder.create()
     }
 
     companion object {
